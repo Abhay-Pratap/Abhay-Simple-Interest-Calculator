971127b0b90241d4807609a750cfba7c270b5b3b
Abhay Pratap Singh <57562486+Abhay-Pratap@users.noreply.github.com>
2021-08-03 11:02:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ function compute()
 
 	if(principal <= 0){
 		alert("Enter a positive number");
-		return;
+		document.getElementById("principal").focus();
+
+		return false;
 	}
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
 
     
     document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
-    document.getElementById("principal").focus();
 }
         
 function updateRate() 
